28e4bad334a795816ab6f7c9604afe40efb273b7
airbender92 <yambo1992@outlook.com>
2025-07-05 21:37:47 +0800

    auth

--- packages/server/src/controllers/auth.controller.ts
+++ packages/server/src/controllers/auth.controller.ts
@@ -23,7 +23,7 @@ export const login = async(req: Request, res: Response) => {
         res.status(200).json({token, refreshToken, userId: user._id});
 
     }catch(error) {
-        res.status(500).json({ error: 'Internal server error'})
+        res.status(500).json({ error: 'Internal server error', errorInfo: error})
     }
 }
 
